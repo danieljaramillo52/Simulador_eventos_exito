1f16f46f0a27bc936311efc0cc911fb3507f4cc7
danieljaramillo52 <daniel.jaramillo@xpertgroup.co>
2025-06-11 16:30:55 -0500

    Creación archivo ejecutable .bat

---
+++ iniciar.bat
@@ -0,0 +1,20 @@
+@echo off
+echo ===============================
+echo Iniciando el proyecto Python...
+echo ===============================
+
+:: Activar el entorno virtual
+: :call venv\Scripts\activate
+
+:: Instalar requerimientos (Comentar si ya están instalados)
+echo Instalando requerimientos...
+pip install -r requirements.txt
+echo ===============================
+
+:: Ejecutar el script principal
+echo Ejecutando el programa...
+Streamlit run main.py
+
+:: Esperar para que no se cierre de inmediato
+echo.
+pause
